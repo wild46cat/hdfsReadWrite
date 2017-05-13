9bf866768c5d7d25bc5312b44d53c13b82beea1d
WuXueyou <409898859@qq.com>
2017-05-13 23:12:03 +0800

    调试阶段

--- src/main/java/com/xueyou/demo/App.java
+++ src/main/java/com/xueyou/demo/App.java
@@ -4,15 +4,7 @@ package com.xueyou.demo;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.fs.permission.FsAction;
-import org.apache.hadoop.fs.permission.FsPermission;
-import org.apache.hadoop.io.IOUtils;
-
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
-import java.net.URI;
 
 /**
  * Hello world!
@@ -23,21 +15,12 @@ public class App {
         String uri = args[0];
         System.out.println(uri);
 //        HDFSUtils.ReadFile(uri, System.out);
+//        HDFSUtils.mkdirs(uri, "/user/xytest/wes", new FsPermission(FsAction.ALL, FsAction.ALL, FsAction.ALL));
+        HDFSUtils.mkdirs(uri, "/user/xytest/cc");
         Configuration configuration = new Configuration();
 
         FSDataOutputStream fsDataOutputStream = null;
         FileSystem fileSystem = null;
         FileInputStream fileInputStream = null;
-
-//        HDFSUtils.mkdirs(uri, "/user/xytest/wes", new FsPermission(FsAction.ALL, FsAction.ALL, FsAction.ALL));
-        HDFSUtils.mkdirs(uri, "/user/xytest/cc");
-
-//        try {
-//            fileSystem = FileSystem.get(URI.create(uri), configuration);
-//            fileSystem.mkdirs(new Path("/user/xytest/cc"));
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        } finally {
-//        }
     }
 }
